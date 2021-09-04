a3ad1017c6081e10afd2a13434f5478c51bcda60
Jomana Sami <jojo.sami.js@gmail.com>
2021-09-04 16:01:09 +0300

    login Breeze

--- database/migrations/2021_09_04_120347_create_queues_table.php
+++ database/migrations/2021_09_04_120347_create_queues_table.php
@@ -20,6 +20,7 @@ class CreateQueuesTable extends Migration
             $table->foreign('user_id')->references('id')->on('users');
             $table->unsignedBigInteger('service_id');
             $table->foreign('service_id')->references('id')->on('services');
+            $table->time('called_at');
             $table->timestamps();
         });
     }
